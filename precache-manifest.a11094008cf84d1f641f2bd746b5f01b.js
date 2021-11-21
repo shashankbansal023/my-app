ad6d6b25cd3a2c19b7aaab8365cf4d42c51d944b
Shashank Bansal <shashankbansal023@gmail.com>
2021-11-21 15:06:51 +0530

    Updates

---
+++ precache-manifest.a11094008cf84d1f641f2bd746b5f01b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce45150e9d6265b882258dafbd987a25",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7293e5059eda7ef22992",
+    "url": "/my-app/static/css/main.19792701.chunk.css"
+  },
+  {
+    "revision": "66c67ec8477dfe93dd1f",
+    "url": "/my-app/static/js/2.20187413.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/my-app/static/js/2.20187413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22691b1ebb5c0285fcce",
+    "url": "/my-app/static/js/3.8bd91e5b.chunk.js"
+  },
+  {
+    "revision": "7293e5059eda7ef22992",
+    "url": "/my-app/static/js/main.bbf25c29.chunk.js"
+  },
+  {
+    "revision": "cd7c779e5c860290be08",
+    "url": "/my-app/static/js/runtime-main.8ac92a85.js"
+  }
+]);
